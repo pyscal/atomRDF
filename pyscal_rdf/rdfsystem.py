3b6e1a5eae715250c22dc3fc78f2635f9c2a704d
Sarath Menon <sarathmenon@mailbox.org>
2023-06-26 00:30:29 +0200

    add a system class

---
+++ pyscal_rdf/rdfsystem.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pyscal3.core as pc
+
+class System(pc.System):
+	def __init__(self, filename = None, 
+			format = "lammps-dump", 
+        	compressed = False, 
+        	customkeys = None):
+		super().__init__(filename = filename, 
+			format = format, 
+        	compressed = compressed, 
+        	customkeys = customkeys)
+		#this is the sample which will be stored
+		self.sample = None
