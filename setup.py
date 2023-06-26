fde95da062170df1322a15834d397f537e64ae82
Sarath Menon <sarathmenon@mailbox.org>
2023-06-26 21:42:34 +0200

    Bump version: 0.0.8 → 0.0.9

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='pyscal_rdf',
-    version='0.0.8',
+    version='0.0.9',
     author='Abril Azocar Guzman, Sarath Menon',
     author_email='sarath.menon@pyscal.org',
     description='Ontology based structural manipulation and quering',
