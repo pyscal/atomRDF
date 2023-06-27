16fe7076b4defa22d0aad7c6cbc7ad3fc0e0a6d0
Sarath Menon <sarathmenon@mailbox.org>
2023-06-28 01:09:21 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='pyscal_rdf',
-    version='0.0.12',
+    version='0.0.13',
     author='Abril Azocar Guzman, Sarath Menon',
     author_email='sarath.menon@pyscal.org',
     description='Ontology based structural manipulation and quering',
