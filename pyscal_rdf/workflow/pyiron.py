ae6221be4ebc511cdb7caa67c62a79a3cb52948b
Sarath Menon <sarathmenon@mailbox.org>
2024-04-09 15:59:58 +0200

    update

--- pyscal_rdf/workflow/pyiron.py
+++ pyscal_rdf/workflow/pyiron.py
@@ -1,7 +1,12 @@
 """
 Wrappers for pyiron jobs
 """
+import os
+from functools import partial, update_wrapper
 import pyscal_rdf.workflow.workflow as wf
+from pyscal_rdf import KnowledgeGraph, System
+
+
 
 def _check_if_job_is_valid(job):
 	valid_jobs = ['Lammps', ]
