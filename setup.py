7fd70291af60e855185b5c8360541ac05caf66d3
Sarath <sarathmenon@mailbox.org>
2024-08-19 11:34:31 +0200

    Bump version: 0.9.14 → 0.9.15

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as readme_file:
 
 setup(
     name='atomrdf',
-    version='0.9.14',
+    version='0.9.15',
     author='Abril Azocar Guzman, Sarath Menon',
     author_email='sarath.menon@pyscal.org',
     description='Ontology based structural manipulation and quering',
