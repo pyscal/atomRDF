4af3565cc7bc50add05287521effbbe1c39deba5
Sarath <sarathmenon@mailbox.org>
2025-02-17 16:40:30 +0100

    copy sim cells reps

--- atomrdf/workflow/workflow.py
+++ atomrdf/workflow/workflow.py
@@ -153,6 +153,7 @@ class Workflow:
             self.kg.change_label(sample, new_label)
             self._get_lattice_properties(parent_sample, sample, structure,)
             self._add_inherited_properties(parent_sample, sample,)
+            self._add_cell_repetitions(parent_sample, sample,)
 
     def _get_lattice_properties(
         self, parent_sample, sample, structure,
@@ -200,7 +201,22 @@ class Workflow:
         ]
 
         structure._add_crystal_structure(targets=targets)
+
+    def _add_cell_repetitions(self, parent_sample, sample):
+        #add lattice repetitions when doing simulations
+
+        if sample is None:
+            return
         
+        old_simcell = self.kg.value(parent_sample, CMSO.hasSimulationCell)
+        x = self.kg.value(old_simcell, CMSO.hasRepetition_x)        
+        y = self.kg.value(old_simcell, CMSO.hasRepetition_y)
+        z = self.kg.value(old_simcell, CMSO.hasRepetition_z)
+
+        new_simcell = self.kg.value(sample, CMSO.hasSimulationCell)
+        self.kg.add((new_simcell, CMSO.hasRepetition_x, x))
+        self.kg.add((new_simcell, CMSO.hasRepetition_y, y))
+        self.kg.add((new_simcell, CMSO.hasRepetition_z, z))
     
     def _add_inherited_properties(
         self, parent_sample, sample,
