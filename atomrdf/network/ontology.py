531e19c4e03d4b5865226e52c0e100306a0d4755
Sarath <sarathmenon@mailbox.org>
2025-03-26 14:46:10 +0100

    update patch

--- atomrdf/network/ontology.py
+++ atomrdf/network/ontology.py
@@ -45,10 +45,10 @@ def read_ontology():
     combo = cmso + pldo + podo + asmo + ldo + cdco
 
     # add namespaces
-    #combo.add_namespace("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
-    #combo.add_namespace("rdfs", "http://www.w3.org/2000/01/rdf-schema#")
+    combo.add_namespace("rdfs", "http://www.w3.org/2000/01/rdf-schema#")
 
     # add extra terms for quering
+    #just a temporary patch for labels
     combo.add_term(
         "http://www.w3.org/2000/01/rdf-schema#label",
         "data_property",
@@ -56,33 +56,11 @@ def read_ontology():
         namespace="rdfs",
         rn = ['str']
     )
-    combo.add_term(
-        "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
-        "object_property",
-        delimiter="#",
-        namespace="rdf",
-    )
-
-    # interontology paths
-    #CMSO -> PODO VACANCY
-    combo.add_path(("cmso:CrystallineMaterial", "cdco:hasCrystallographicDefect", "pldo:PlanarDefect"))
-    combo.add_path(("cmso:CrystallineMaterial", "cdco:hasCrystallographicDefect", "podo:Vacancy"))
-    combo.add_path(("cmso:ComputationalSample", "podo:hasVacancyConcentration", "float"))
-    combo.add_path(("cmso:ComputationalSample", "podo:hasNumberOfVacancies", "int"))
     
-    #CMSO -> PODO IMPURITY
-    combo.add_path(("cmso:CrystallineMaterial", "cdco:hasCrystallographicDefect", "podo:SubstitutionalImpurity"))
-    combo.add_path(("cmso:CrystallineMaterial", "cdco:hasCrystallographicDefect", "podo:InterstitialImpurity"))
-    #combo.add_path(("podo:InterstitialImpurity", "rdfs:label", "string"))
-    combo.add_path(("cmso:ComputationalSample", "podo:hasNumberOfImpurityAtoms", "int"))
-    combo.add_path(("cmso:ComputationalSample", "podo:hasImpurityConcentration", "float"))
-
-    #CMSO -> LDO DISL paths
-    combo.add_path(("cmso:CrystallineMaterial", "cdco:hasCrystallographicDefect", "ldo:LineDefect"))
-    combo.add_path(("cmso:CrystallineMaterial", "cdco:hasCrystallographicDefect", "ldo:Dislocation"))
-
-    #CMSO -> PODO DISL paths
-    combo.add_path(("cmso:CrystallineMaterial", "cdco:hasCrystallographicDefect", "pldo:GrainBoundary"))
+    combo.add_path(("asmo:CalculatedProperty", "rdfs:label", "string"))
+    combo.add_path(("asmo:InputParameter", "rdfs:label", "string"))
+    combo.add_path(("prov:SoftwareAgent", "rdfs:label", "string"))
+    combo.add_path(("asmo:InteratomicPotential", "rdfs:label", "string"))
     
     # return
     return combo
