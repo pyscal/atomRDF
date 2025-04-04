e2270426d97d8a9754c6e2d2c33a49358fb2e30a
Sarath <sarathmenon@mailbox.org>
2025-04-04 10:45:24 +0200

    fix store

--- tests/test_store.py
+++ tests/test_store.py
@@ -3,8 +3,8 @@ import os
 from atomrdf import KnowledgeGraph, System
 from atomrdf.namespace import CMSO, PLDO
 import shutil
-
+import atomrdf.build as build
 
 def test_sqlalchemy():
 	s = KnowledgeGraph(store='SQLAlchemy', store_file='aa')
-	sys = System.create.element.Fe(graph=s)
+	sys = build.bulk("Fe", graph=s)
